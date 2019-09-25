97a6e00719beb6641fec7c0d762be44507744164
ginnac <ginnacampbell1990@gmail.com>
2019-09-25 16:50:18 -0600

    Updates

---
+++ precache-manifest.38df9d86413427e2e59a654e2759c917.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe11918c488ea7c5433c3211bb38d4d5",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "23a66ff03b9c0678009e",
+    "url": "/clicky-game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "846eef8db1df4732ef13",
+    "url": "/clicky-game/static/js/2.50bf3734.chunk.js"
+  },
+  {
+    "revision": "23a66ff03b9c0678009e",
+    "url": "/clicky-game/static/js/main.d337554a.chunk.js"
+  },
+  {
+    "revision": "18967ef35086d38857e0",
+    "url": "/clicky-game/static/js/runtime-main.5319c75b.js"
+  }
+]);
